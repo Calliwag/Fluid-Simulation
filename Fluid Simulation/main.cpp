b9cde054f509dd2ca9921ad566421ffc60e59841
Calvin Akom <calvinakom@gmail.com>
2024-09-27 01:12:19 -0400

    Fixed intellisense with murder

--- Fluid Simulation/main.cpp
+++ Fluid Simulation/main.cpp
@@ -12,7 +12,6 @@ int main(int argc, char* argv[])
 	*/
 
 	argparse::ArgumentParser program("FluidSimulation");
-
 	program.add_argument("--Vorticity")
 		.help("Set the vorticity of the fluid.")
 		.scan<'g', double>()
